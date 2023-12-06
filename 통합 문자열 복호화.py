0441d53c4830b0786868c041661dca45ed4b6fcd
chaewon314 <maumsan0623@gmail.com>
2023-12-06 09:53:49 +0900

    add alphabet index shell function

--- 통합 문자열 복호화.py
+++ 통합 문자열 복호화.py
@@ -55,14 +55,14 @@ def string_arrange():
     # dic={'~':28,' ':27,'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,
     #      'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,
     #      'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25}
-    dic='abcdefghijklmnopqrstuvwxyz ~'                  #알파벳 대응표
+    dic='abcdefghijklmnopqrstuvwxyz ~!1234567890ㄱㄴㄷㄹㅁㅍㅅㅇㅈㅊㅋㅌㅍㅎ'                  #알파벳 대응표
 
     f=open('dec.txt','r')
 
 
     while boolean:
         a=f.readline()         #a는 dec.txt 파일을 한줄씩 입력받음
-        
+       
         if str(a)=='':              #블록화 복호문을 모두 함수에 대입한 후, ''를 만나면 boolean으로 while문 탈출, if문 탈출
             boolean=0
             break
@@ -76,7 +76,7 @@ def string_arrange():
                     k-=(len(dic)**(blockN-x))*(len(dic)-y)
                     break
                     
-
+        l=l.replace('~','')
         string+=l
     print('최종 복호문: '+string)
     
